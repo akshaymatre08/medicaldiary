68dfbf2836c64729804637106f910ebca63304da
Akshay Shivaji Matre <92999927+akshaymatre08@users.noreply.github.com>
2022-07-09 00:52:56 +0530

    constant.php updated

--- web/include/Constants.php
+++ web/include/Constants.php
@@ -1,5 +1,5 @@
 <?php
-define('API_URL', 'http://localhost/medicaldiary/api/public/');		//Don't Forgot to put slash at the end of url
+define('API_URL', 'http://localhost/medicaldiary/api/public/');		
 define('WEBSITE_NAME', 'Medical Diary');
 
 define('ERROR', 'error');
